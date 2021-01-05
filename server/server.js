ee5204addb8beef3582111a0fcfe02e3e11be429
Jordan Roberts <jdrobs19@gmail.com>
2021-01-05 11:02:48 -0700

    integrated front end

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 //import ApolloServer
 const {ApolloServer} = require('apollo-server-express');
@@ -21,6 +23,15 @@ server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//serve up static assests
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
