16921f4fdc9ad0eb361bb3a4151b32fdcc6c5589
Jordan Roberts <jdrobs19@gmail.com>
2021-01-07 15:42:06 -0700

    add friend and thought

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,12 +3,17 @@ import { Redirect, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { ADD_FRIEND, ADD_USER } from '../utils/mutations';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 
 const Profile = () => {
+
+  const [addFriend] = useMutation(ADD_FRIEND);
+
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -17,21 +22,31 @@ const Profile = () => {
 
   const user = data?.me || data?.user || {};
 
-  if(Auth.loggedIn() && Auth.getProfile().data.username === userParam){
-    return <Redirect to='/profile'/>
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Redirect to='/profile' />
   }
 
   if (loading) {
     return <div>Loading...</div>
   }
 
-  if(!user?.username){
-    return(
+  if (!user?.username) {
+    return (
       <h4>
         You need to be logged in to see this page. User the navigation links above to sign up or log in!
       </h4>
     );
   }
+
+  const handleClick = async () => {
+    try{
+      await addFriend({
+        variables: {id: user._id}
+      });
+    }catch(e){
+      console.error(e);
+    }
+  };
 
   return (
     <div>
@@ -39,6 +54,11 @@ const Profile = () => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+        {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>
+          Add Friend
+</button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -54,6 +74,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm/>}</div>
     </div>
   );
 };
